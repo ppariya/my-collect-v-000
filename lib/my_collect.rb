4991150deb81b38f10971e98150ae6eed8d047b0
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-05 07:21:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def collect(array)
   i = 0
-  my_collect = []
+  my_new_collect = []
   while i > array.length
     my_collect << (array[i])
     yield(array[i])
     i += 1
   end
-  my_collect
+  my_new_collect
 end
